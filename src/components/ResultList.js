c715343d352542ad6b397ea12119a7d41b3310d5
P1811 <p1811@student.jamk.fi>
2020-10-09 16:36:55 +0300

    Added alert functionality

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,5 +1,5 @@
 import React  from "react";
-import {View, Text, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, FlatList, StyleSheet, TouchableOpacity, Alert} from 'react-native';
 import ResultsDetails from '../components/ResultsDetail';
 import * as axios from 'axios';
 
@@ -17,6 +17,22 @@ const ResultList = ({title, recipes,hasError}) => {
  
   }
 
+  const createTwoButtonAlert = (param) =>
+  Alert.alert(
+    "You are going to delete this recipe",
+    "Are you sure you wanna delete your mamma",
+    [
+      {
+        text: "Cancel",
+        onPress: () => {},
+        style: "cancel"
+      },
+      { text: "OK", onPress: () =>deleteRecipe(param.id) }
+    ],
+    { cancelable: false }
+  );
+
+
 
     return (
       <View style={{marginTop:50}}>
@@ -27,7 +43,7 @@ const ResultList = ({title, recipes,hasError}) => {
             data={recipes}
             keyExtractor={(item) => item.id.toString()}
             renderItem={({item}) => { return(
-                <TouchableOpacity onPress={()=>('ShowMethod',{id: item.id})} onLongPress={()=>deleteRecipe(item.id)} >
+                <TouchableOpacity onPress={()=>('ShowMethod',{id: item.id})} onLongPress={()=>createTwoButtonAlert(item)} >
               <View>
                   <ResultsDetails result={item}/>
               </View>
